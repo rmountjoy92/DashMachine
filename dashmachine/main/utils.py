55ffaaf13da741cdb98cf651b8bf9b4c40a4640f
Ross Mountjoy <ross.mountjoy@gmail.com>
2020-02-01 18:56:11 -0500

    fixed bug with static links

--- dashmachine/main/utils.py
+++ dashmachine/main/utils.py
@@ -170,14 +170,18 @@ def dashmachine_init():
 
     # create the user_data subdirectories, link them to static
     user_backgrounds_folder = os.path.join(user_data_folder, "backgrounds")
+    backgrounds_folder = os.path.join(images_folder, "backgrounds")
     if not os.path.isdir(user_backgrounds_folder):
         os.mkdir(user_backgrounds_folder)
-        os.symlink(user_backgrounds_folder, os.path.join(images_folder, "backgrounds"))
+    if not os.path.isdir(backgrounds_folder):
+        os.symlink(user_backgrounds_folder, backgrounds_folder)
 
-    icons_folder = os.path.join(user_data_folder, "icons")
+    user_icons_folder = os.path.join(user_data_folder, "icons")
+    icons_folder = os.path.join(images_folder, "icons")
+    if not os.path.isdir(user_icons_folder):
+        os.mkdir(user_icons_folder)
     if not os.path.isdir(icons_folder):
-        os.mkdir(icons_folder)
-        os.symlink(icons_folder, os.path.join(images_folder, "icons"))
+        os.symlink(user_icons_folder, icons_folder)
 
     config_file = os.path.join(user_data_folder, "config.ini")
     if not os.path.exists(config_file):
