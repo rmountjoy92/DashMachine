071d12a28505c200b3877c3dc1a16c02e7853051
Ross Mountjoy <ross.mountjoy@gmail.com>
2020-02-08 09:12:40 -0500

    working on user system

--- dashmachine/user_system/utils.py
+++ dashmachine/user_system/utils.py
@@ -2,14 +2,12 @@ from dashmachine import db, bcrypt
 from dashmachine.user_system.models import User
 
 
-def add_edit_user(username, password, user_id=None, role=None, new=False):
+def add_edit_user(username, password, user_id=None, role=None):
     if user_id:
         user = User.query.filter_by(id=user_id).first()
-    elif new:
-        user = User()
+        if not user:
+            user = User()
     else:
-        user = User.query.first()
-    if not user:
         user = User()
 
     hashed_password = bcrypt.generate_password_hash(password).decode("utf-8")
