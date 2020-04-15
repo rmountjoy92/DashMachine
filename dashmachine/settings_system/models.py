838a6ae458c93cd6a5a4130dc18839f1774768ee
Felix Nüsse <felix.nuesse@t-online.de>
2020-04-15 12:09:15 +0200

    implement layout-setting in backend

--- dashmachine/settings_system/models.py
+++ dashmachine/settings_system/models.py
@@ -5,6 +5,7 @@ class Settings(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     theme = db.Column(db.String())
     accent = db.Column(db.String())
+    layout = db.Column(db.String())
     background = db.Column(db.String())
     roles = db.Column(db.String())
     home_access_groups = db.Column(db.String())
