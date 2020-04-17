ffbba98061f0d3b25b0d1eee21b4cbbd72dd4a66
sirpxl <37268984+sirpxl@users.noreply.github.com>
2020-04-17 09:25:48 -0400

    Add cli flag to change default port

--- run.py
+++ run.py
@@ -2,7 +2,13 @@
 
 import os
 import pathlib
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-p", "--port", help='port to listen to (default "0")', type=int)
+args = parser.parse_args()
+host_port = args.port
+if host_port == None: host_port = 5000
 
 root_folder = pathlib.Path().absolute()
 if not os.path.isdir(os.path.join(root_folder, "dashmachine", "user_data")):
@@ -20,4 +26,4 @@ from dashmachine.main.utils import dashmachine_init
 dashmachine_init()
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=True, host="0.0.0.0", threaded=True)
+    app.run(debug=True, use_reloader=True, host="0.0.0.0", port=host_port, threaded=True)
